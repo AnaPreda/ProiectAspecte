97625ec68ad1baa1e6f7e427f758c2dd3ce2f2f2
Ana Preda <preda_ana20@yahoo.com>
2020-03-22 10:03:26 +0200

    Added dummy data and passed it to the template

--- proiect/views.py
+++ proiect/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'CoreyMs',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    return render(request, 'proiect/home.html')
+    context={
+        'posts': posts
+    }
+    return render(request, 'proiect/home.html', context)
 
 def about(request):
-    return render(request, 'proiect/about.html')
+    return render(request, 'proiect/about.html',{'title': 'About'})
