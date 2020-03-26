02648d74dedd5d1558e353a2a4df0b4cdc1f2274
Cimpineanu Dragos <dragos@firebyte.ro>
2020-03-26 20:56:36 +0200

    Implemented create post functionality

--- proiect/views.py
+++ proiect/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView)
 
 
 def home(request):
@@ -19,6 +21,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
