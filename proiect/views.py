d6ef235277ccf2d53ebbd9ec1b068b614136a705
Cimpineanu Dragos <dragos@firebyte.ro>
2020-03-26 20:19:09 +0200

    Created post detail view

--- proiect/views.py
+++ proiect/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
-    context={
+    context = {
         'posts': Post.objects.all()
     }
     return render(request, 'proiect/home.html', context)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'proiect/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
 def about(request):
-    return render(request, 'proiect/about.html',{'title': 'About'})
+    return render(request, 'proiect/about.html', {'title': 'About'})
