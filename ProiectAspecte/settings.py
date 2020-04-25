ccc7d538ea050b916d471d3a2b42de76e956292a
Ana Preda <preda_ana20@yahoo.com>
2020-04-25 20:24:53 +0300

    Changed settings.py, region added

--- ProiectAspecte/settings.py
+++ ProiectAspecte/settings.py
@@ -151,7 +151,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_REGION_NAME = 'us-west-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
